475661e9d9230aa1d2e4408deae10d5b92dabce8
LuisAlberto2304 <0323105921@ut-tijuana.edu.mx>
2026-02-03 13:53:59 -0800

    primer commit

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/map")
-def map():
+def map_view():
     return render_template("map.html")
+
+@app.route("/guardar_punto", methods=["POST"])
+def guardar_punto():
+    data = request.json
+    print(data)  # {'lat': ..., 'lng': ...}
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
